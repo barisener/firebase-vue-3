a1c7e557cb0de058b4d4df4b9aadef03ec87de77
Barış Şener <baris@dealforward.com>
2024-03-09 02:27:40 +0300

    config changed

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,18 @@
 <template>
   <q-page class="flex flex-center">
+    <div class="bg-red-800 size-6">title</div>
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
